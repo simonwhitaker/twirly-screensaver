91738a16be9476bb1aa000ad15eb1a841e3f94de
Simon Whitaker <simon@goosoftware.co.uk>
2011-09-26 23:58:32 -0500

    Initial Commit

---
+++ GSTwirly/GSTwirlyView.h
@@ -0,0 +1,13 @@
+//
+//  GSTwirlyView.h
+//  GSTwirly
+//
+//  Created by Simon Whitaker on 26/09/2011.
+//  Copyright 2011 Goo Software Ltd. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface GSTwirlyView : ScreenSaverView
+
+@end
---
+++ GSTwirly/GSTwirlyView.m
@@ -0,0 +1,52 @@
+//
+//  GSTwirlyView.m
+//  GSTwirly
+//
+//  Created by Simon Whitaker on 26/09/2011.
+//  Copyright 2011 Goo Software Ltd. All rights reserved.
+//
+
+#import "GSTwirlyView.h"
+
+@implementation GSTwirlyView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
