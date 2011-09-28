fbef04bf4de8a037b2afc7473c42108d28e11ef1
Simon Whitaker <simon@goosoftware.co.uk>
2011-09-28 01:04:25 +0100

    Add UNLICENSE

--- GSTwirly/GSTwirlyView.h
+++ GSTwirly/GSTwirlyView.h
@@ -3,7 +3,7 @@
 //  GSTwirly
 //
 //  Created by Simon Whitaker on 26/09/2011.
-//  Copyright 2011 Goo Software Ltd. All rights reserved.
+//  License: see UNLICENSE
 //
 
 #import <ScreenSaver/ScreenSaver.h>
--- GSTwirlyPreview/GSTwirlyPreviewAppDelegate.m
+++ GSTwirlyPreview/GSTwirlyPreviewAppDelegate.m
@@ -3,7 +3,7 @@
 //  GSTwirlyPreview
 //
 //  Created by Simon Whitaker on 27/09/2011.
-//  Copyright 2011 Goo Software Ltd. All rights reserved.
+//  License: see UNLICENSE
 //
 
 #import "GSTwirlyPreviewAppDelegate.h"
